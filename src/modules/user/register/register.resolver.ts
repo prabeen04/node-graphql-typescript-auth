601e6c3c44e2782f5098a2d4dca5e440d8b5679d
Prabeen04 <prabeen.strange@gmail.com>
2019-06-02 23:21:29 +0530

    bug fixes

--- src/modules/user/register/register.resolver.ts
+++ src/modules/user/register/register.resolver.ts
@@ -1,5 +1,5 @@
 
-export const registerResolver: any = {
+export const resolvers: any = {
     Mutation: {
         register: async (_: any, args: any, ctx, info) => {
             console.log(args)
