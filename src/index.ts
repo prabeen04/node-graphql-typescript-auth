2678d06c65b864cb1becf4f2c830c39e3001aacd
Prabeen04 <prabeen.strange@gmail.com>
2019-06-02 22:48:40 +0530

    generic schema generator created

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
+import { genSchema } from './schema';
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { GraphQLServer } from 'graphql-yoga'
 import * as dotenv from 'dotenv';
-import { resolvers } from './modules/user/userResolver';
 dotenv.config()
 
 async function startServer() {
     try {
         const connection = await createConnection()
-        const server = new GraphQLServer({ typeDefs, resolvers })
+        const server = new GraphQLServer({
+            schema: genSchema() as any
+        })
         server.start(() => console.log('Server is running on localhost:4000'))
     } catch (error) {
         console.log('Error occoured ', error)
