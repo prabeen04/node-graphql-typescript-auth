8789136c9dcfa420690d8e605f106440b10f3608
Prabeen04 <prabeen.strange@gmail.com>
2019-06-02 23:35:46 +0530

    gql2ts added

---
+++ src/scripts/createGQLTypes.ts
@@ -0,0 +1,14 @@
+import { generateNamespace } from "@gql2ts/from-schema";
+import * as fs from "fs";
+import * as path from "path";
+import { genSchema } from './../schema';
+
+const typescriptTypes = generateNamespace("GQL", genSchema());
+
+fs.writeFile(
+    path.join(__dirname, "../types/schema.d.ts"),
+    typescriptTypes,
+    err => {
+        console.log('test', err);
+    }
+);
