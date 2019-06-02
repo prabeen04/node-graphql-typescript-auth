965d7b4736959298b044842c1442221ce04dc8b9
Prabeen04 <prabeen.strange@gmail.com>
2019-06-02 22:17:27 +0530

    grapql-yoga stater server

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
+import { GraphQLServer } from 'graphql-yoga'
 import * as dotenv from 'dotenv';
 dotenv.config()
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: (_: any, { name }: any) => `Hello ${name || 'World'}`,
+  },
+}
+
 async function startServer() {
     try {
         const connection = await createConnection()
-        console.log('server started again ...')
-
+        const server = new GraphQLServer({ typeDefs, resolvers })
+        server.start(() => console.log('Server is running on localhost:4000'))
     } catch (error) {
         console.log('Error occoured ', error)
     }
