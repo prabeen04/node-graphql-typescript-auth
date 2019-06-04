d087391c823fc2a4171db7f0a29a0cdfa0560370
Prabeen04 <prabeen.strange@gmail.com>
2019-06-04 22:13:33 +0530

    gql2ts bug fix

--- src/modules/user/register/register.resolver.ts
+++ src/modules/user/register/register.resolver.ts
@@ -2,11 +2,10 @@ import { User } from "../../../entity/User.entity";
 
 export const resolvers: any = {
     Mutation: {
-        register: async (_: any, args: any, ctx, info) => {
+        register: async (_: any, args: GQL.IRegisterOnMutationArguments, ctx: any, info: any) => {
             try {
                 const user = User.create(args)
-                const newUser = await user.save()
-                console.log(newUser)
+                await user.save()
                 return true
             } catch (error) {
                 console.log(error)
