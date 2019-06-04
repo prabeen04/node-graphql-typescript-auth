2d4bac411d946671d91c9a908c9054eb4aa81e00
Prabeen04 <prabeen.strange@gmail.com>
2019-06-04 21:48:16 +0530

    try/catch added for user register

--- src/modules/user/register/register.resolver.ts
+++ src/modules/user/register/register.resolver.ts
@@ -3,11 +3,15 @@ import { User } from "../../../entity/User.entity";
 export const resolvers: any = {
     Mutation: {
         register: async (_: any, args: any, ctx, info) => {
-            console.log(args)
-            const user = User.create(args)
-            const newUser = await user.save()
-            console.log(newUser)
-            return false
+            try {
+                const user = User.create(args)
+                const newUser = await user.save()
+                console.log(newUser)
+                return true
+            } catch (error) {
+                console.log(error)
+                return false
+            }
         }
     }
 }
