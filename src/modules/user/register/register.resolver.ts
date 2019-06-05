c1e4d21df3e6860fd5dcf21782b518119d9b9b2b
Prabeen04 <prabeen.strange@gmail.com>
2019-06-05 08:59:42 +0530

    register yup schema created

--- src/modules/user/register/register.resolver.ts
+++ src/modules/user/register/register.resolver.ts
@@ -1,9 +1,24 @@
 import { User } from "../../../entity/User.entity";
-
+import * as yup from 'yup'
+const registerSchema = yup.object().shape({
+    email: yup
+        .string()
+        .min(3, 'Email not long enough')
+        .max(255)
+        .email('Must provide a valid email')
+        .required('Email is required'),
+    password: yup
+        .string()
+        .min(6, 'Password should have min 6 char long')
+        .max(255)
+        .required('Password is required')
+})
 export const resolvers: any = {
     Mutation: {
         register: async (_: any, args: GQL.IRegisterOnMutationArguments, ctx: any, info: any) => {
             try {
+                const errors = await registerSchema.validate(args)
+                console.log(errors)
                 const user = User.create(args)
                 await user.save()
                 return true
