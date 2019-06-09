2ba9e3b1cee228e8deed61d2e5a4b4120bb3e0b7
Prabeen04 <prabeen.strange@gmail.com>
2019-06-09 19:53:11 +0530

    ormconfig for test created

--- ormconfig.js
+++ ormconfig.js
@@ -1,24 +1,54 @@
-module.exports = {
-    "type": "postgres",
-    "host": process.env.POSTGRES_HOST,
-    "port": 5432,
-    "username": process.env.POSTGRES_USERNAME,
-    "password": process.env.POSTGRES_PASSWORD,
-    "database": process.env.POSTGRES_DATABASE,
-    "synchronize": true,
-    "logging": false,
-    "entities": [
-        "src/entity/**/*.ts"
-    ],
-    "migrations": [
-        "src/migration/**/*.ts"
-    ],
-    "subscribers": [
-        "src/subscriber/**/*.ts"
-    ],
-    "cli": {
-        "entitiesDir": "src/entity",
-        "migrationsDir": "src/migration",
-        "subscribersDir": "src/subscriber"
-    }
-}
+module.exports = [
+    {
+        "name": "default"
+        "type": "postgres",
+        "host": process.env.POSTGRES_HOST,
+        "port": 5432,
+        "username": process.env.POSTGRES_USERNAME,
+        "password": process.env.POSTGRES_PASSWORD,
+        "database": process.env.POSTGRES_DATABASE,
+        "synchronize": true,
+        "logging": false,
+        "entities": [
+            "src/entity/**/*.ts"
+        ],
+        "migrations": [
+            "src/migration/**/*.ts"
+        ],
+        "subscribers": [
+            "src/subscriber/**/*.ts"
+        ],
+        "cli": {
+            "entitiesDir": "src/entity",
+            "migrationsDir": "src/migration",
+            "subscribersDir": "src/subscriber"
+        }
+    },
+    {
+        "name": "test"
+        "type": "postgres",
+        "host": process.env.POSTGRES_HOST_TEST,
+        "port": 5432,
+        "username": process.env.POSTGRES_USERNAME_TEST,
+        "password": process.env.POSTGRES_PASSWORD_TEST,
+        "database": process.env.POSTGRES_DATABASE_TEST,
+        "synchronize": true,
+        "logging": false,
+        "dropSchema": true,
+        "entities": [
+            "src/entity/**/*.ts"
+        ],
+        "migrations": [
+            "src/migration/**/*.ts"
+        ],
+        "subscribers": [
+            "src/subscriber/**/*.ts"
+        ],
+        "cli": {
+            "entitiesDir": "src/entity",
+            "migrationsDir": "src/migration",
+            "subscribersDir": "src/subscriber"
+        }
+    },
+
+]
