76c01a72b398a5278547c54ae24ab8d38c603cf7
Prabeen04 <prabeen.strange@gmail.com>
2019-06-12 19:26:45 +0530

    mandrill-api removed

--- src/utils/createTypeormConn.ts
+++ src/utils/createTypeormConn.ts
@@ -3,6 +3,5 @@ import { getConnectionOptions, createConnection } from "typeorm";
 
 export const createTypeormConn = async () => {
   const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
-  console.log(connectionOptions)
   return createConnection({...connectionOptions, name: "default"});
 };
