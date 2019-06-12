6e5489760fa1450c40144c3710a1848dc5ceec71
Prabeen04 <prabeen.strange@gmail.com>
2019-06-12 20:07:57 +0530

    unused mailoption interface deleted

--- src/utils/SendEmail.ts
+++ src/utils/SendEmail.ts
@@ -1,14 +1,8 @@
 import * as nodemailer from 'nodemailer';
 import { MailOptions } from 'nodemailer/lib/sendmail-transport';
 
-export interface IMailOption {
-    to: string;
-    from: string;
-    subject?: string;
-    text?: string;
-    html?: string | any
-}
 export function sendEmail(mailOption: MailOptions, cb: any): void {
+    console.log(mailOption)
     const transport = nodemailer.createTransport({
         service: 'gmail',
         auth: {
