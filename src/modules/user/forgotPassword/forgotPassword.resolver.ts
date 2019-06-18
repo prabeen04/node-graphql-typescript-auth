fec53469fe116e342cad29e4464533badcc3ade3
Prabeen04 <prabeen.strange@gmail.com>
2019-06-18 19:38:57 +0530

    reset password flow complete

--- src/modules/user/forgotPassword/forgotPassword.resolver.ts
+++ src/modules/user/forgotPassword/forgotPassword.resolver.ts
@@ -1,3 +1,4 @@
+import { User } from './../../../entity/User.entity';
 import { sendEmail } from "../../../utils/SendEmail";
 import { MailOptions } from "nodemailer/lib/sendmail-transport";
 import * as jwt from 'jsonwebtoken';
@@ -6,12 +7,18 @@ export const resolvers: any = {
     Mutation: {
         forgotPassword: async (_: any, args: GQL.IForgotPasswordOnMutationArguments, ctx: any, info: any) => {
             const { email } = args;
+
+            //check if a user exists with the given email
+            const user = User.findOne({ where: { email } })
+            if(!user ) return false;
+
+            //generate a token and send email for reseting password
             const token = jwt.sign({ email }, process.env.CLIENT_SECRET)
             console.log(token)
             const template = `
                 <p>Click on the link below to reset your password</p>
-                <a target='_blank' href='http://localhost:3000/forgotPassword/${token}'>
-                http://localhost:3000/forgotPassword/${token}</a>
+                <a target='_blank' href='http://localhost:3000/resetPassword/${token}'>
+                http://localhost:3000/resetPassword/${token}</a>
                 `
             const mailOption: MailOptions = {
                 to: args.email,
