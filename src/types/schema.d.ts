3239c06d6dbbb06469a0e746138c150f7ff4a062
Prabeen04 <prabeen.strange@gmail.com>
2019-06-20 21:14:46 +0530

    small stuff

--- src/types/schema.d.ts
+++ src/types/schema.d.ts
@@ -2,83 +2,83 @@
 // graphql typescript definitions
 
 declare namespace GQL {
-interface IGraphQLResponseRoot {
-data?: IQuery | IMutation;
-errors?: Array<IGraphQLResponseError>;
-}
+    interface IGraphQLResponseRoot {
+        data?: IQuery | IMutation;
+        errors?: Array<IGraphQLResponseError>;
+    }
 
-interface IGraphQLResponseError {
-/** Required for all errors */
-message: string;
-locations?: Array<IGraphQLResponseErrorLocation>;
-/** 7.2.2 says 'GraphQL servers may provide additional entries to error' */
-[propName: string]: any;
-}
+    interface IGraphQLResponseError {
+        /** Required for all errors */
+        message: string;
+        locations?: Array<IGraphQLResponseErrorLocation>;
+        /** 7.2.2 says 'GraphQL servers may provide additional entries to error' */
+        [propName: string]: any;
+    }
 
-interface IGraphQLResponseErrorLocation {
-line: number;
-column: number;
-}
+    interface IGraphQLResponseErrorLocation {
+        line: number;
+        column: number;
+    }
 
-interface IQuery {
-__typename: "Query";
-hello: string | null;
-}
+    interface IQuery {
+        __typename: "Query";
+        hello: string | null;
+    }
 
-interface IMutation {
-__typename: "Mutation";
-changePassword: Array<IError> | null;
-forgotPassword: boolean;
-login: Array<IError> | null;
-register: Array<IError> | null;
-verifyEmail: boolean;
-}
+    interface IMutation {
+        __typename: "Mutation";
+        changePassword: Array<IError> | null;
+        forgotPassword: boolean;
+        login: Array<IError> | null;
+        register: Array<IError> | null;
+        verifyEmail: boolean;
+    }
 
-interface IChangePasswordOnMutationArguments {
-id: string;
-oldPassword: string;
-newPassword: string;
-}
+    interface IChangePasswordOnMutationArguments {
+        id: string;
+        oldPassword: string;
+        newPassword: string;
+    }
 
-interface IForgotPasswordOnMutationArguments {
-email: string;
-}
+    interface IForgotPasswordOnMutationArguments {
+        email: string;
+    }
 
-interface ILoginOnMutationArguments {
-email: string;
-password: string;
-}
+    interface ILoginOnMutationArguments {
+        email: string;
+        password: string;
+    }
 
-interface IRegisterOnMutationArguments {
-firstName?: string | null;
-middleName?: string | null;
-lastName: string;
-email: string;
-password: string;
-dob?: string | null;
-phoneNo?: string | null;
-}
+    interface IRegisterOnMutationArguments {
+        firstName?: string | null;
+        middleName?: string | null;
+        lastName: string;
+        email: string;
+        password: string;
+        dob?: string | null;
+        phoneNo?: string | null;
+    }
 
-interface IVerifyEmailOnMutationArguments {
-token: string;
-}
+    interface IVerifyEmailOnMutationArguments {
+        token: string;
+    }
 
-interface IError {
-__typename: "Error";
-path: string;
-message: string;
-}
+    interface IError {
+        __typename: "Error";
+        path: string;
+        message: string;
+    }
 
-interface IUser {
-__typename: "User";
-id: string;
-firstName: string | null;
-middleName: string | null;
-lastName: string;
-email: string;
-phoneNo: string | null;
-dob: string | null;
-}
+    interface IUser {
+        __typename: "User";
+        id: string;
+        firstName: string | null;
+        middleName: string | null;
+        lastName: string;
+        email: string;
+        phoneNo: string | null;
+        dob: string | null;
+    }
 }
 
 // tslint:enable
