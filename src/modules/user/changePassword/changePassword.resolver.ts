a184deb77bf788e35ed541a5a55e44018beb0014
Prabeen04 <prabeen.strange@gmail.com>
2019-06-20 20:36:27 +0530

    changePassword resolver updated

--- src/modules/user/changePassword/changePassword.resolver.ts
+++ src/modules/user/changePassword/changePassword.resolver.ts
@@ -1,7 +1,21 @@
+import * as bcrypt from 'bcryptjs'
+import { User } from './../../../entity/User.entity';
 export const resolvers: any = {
     Mutation: {
         changePassword: async (_: any, args: GQL.IChangePasswordOnMutataionArguments, ctx: any, info: any) => {
             console.log(args)
+            const { id, oldPassword, newPassword } = args;
+            const user = await User.findOne({
+                where: { id }
+            })
+            if (!user) return false
+            const password = user.password;
+            if (password !== oldPassword) return false;
+            const newHashedPassword = await bcrypt.hash(newPassword, 10)
+            await User.update({ id }, {
+                password: newHashedPassword
+            })
+            return true
         }
     }
 }
